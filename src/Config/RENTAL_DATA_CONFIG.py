421b73f0c29162fb4cd6fe1f915f0772bdaef173
Michael Perillo <msp501@nyu.edu>
2022-07-21 19:22:32 -0400

    Update data config

--- src/Config/RENTAL_DATA_CONFIG.py
+++ src/Config/RENTAL_DATA_CONFIG.py
@@ -1,1 +1,8 @@
 TRULIA_FIRST_HEADERS = ['streetAddress', 'addressLocality', 'addressRegion', 'postalCode', 'url']
+
+TRULIA_HOUSE_METRICS_ATTRS = {
+    'beds' : {'src': 'https://www.trulia.com/images/icons/txl3/BedIcon.svg'},
+    'baths': {'src': 'https://www.trulia.com/images/icons/txl3/BathIcon.svg'},
+    'sqft': {'src': 'https://www.trulia.com/images/icons/txl3/SquareFeetIcon.svg'},
+    'h3-data': {'data-testid': 'on-market-price-details'}
+}
