a4c3e8c4fb40d23faea08e1614f1a08faea5b3a7
Michael Perillo <msp501@nyu.edu>
2022-07-22 15:56:20 -0400

    Parse listing details using new functions

--- src/Scripts/trulia_rental_scrape.py
+++ src/Scripts/trulia_rental_scrape.py
@@ -1,10 +1,15 @@
 import sys
 import json
+import time
+from typing import Callable, Optional
+
 import pandas as pd
+from selenium.webdriver.chrome.options import Options
 
 from src.Config.DRIVER_CACHE_DIRECTORY import CACHE_DIR
 from src.Config.RENTAL_URLS import URL_DICT
 from src.Config.RENTAL_DATA_CONFIG import TRULIA_FIRST_HEADERS, TRULIA_HOUSE_METRICS_ATTRS
+from src.Classes.Home_Listing import HomeListing
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -56,8 +61,8 @@ def appliance_header_location(tag: Tag) -> bool:
            and tag.text.find('Appliances') != -1
 
 
-
 def find_appliances_html(soup: BeautifulSoup) -> Tag:
+    # Todo: Change this over to using the feature list location generator
     """
     Return the cells listing the appliances
     :param soup: Soup object representing the home's page
@@ -65,12 +70,40 @@ def find_appliances_html(soup: BeautifulSoup) -> Tag:
     """
     return soup.find(appliance_header_location).find_next('span')
 
+
 def parse_appliances(soup: BeautifulSoup) -> list:
     appliance_tag = find_appliances_html(soup)
     out_list = []
     while appliance_tag is not None:
         out_list.append(appliance_tag.text)
         appliance_tag = appliance_tag.find_next_sibling()
+    return out_list
+
+
+def generate_feature_list_locator(first_word_list_title: str) -> Callable[[Tag], bool]:
+    def feature_list_locator(tag: Tag) -> bool:
+        return tag.name == 'div' \
+               and tag.attrs.get('data-testid') is None \
+               and tag.attrs.get('class') is not None \
+               and tag.attrs['class'][0].find('Text__TextBase') != -1 \
+               and tag.text.find(first_word_list_title) != -1
+    return feature_list_locator
+
+
+def find_detail_tag(detail, soup) -> Optional[Tag]:
+    detail_location = generate_feature_list_locator(detail)
+    detail_tag = soup.find(detail_location)
+    if detail_tag is not None:
+        return detail_tag.find_next('span')
+    return None
+
+
+def parse_detail_feature_list(detail: str, soup: BeautifulSoup) -> list[str]:
+    detail_tag = find_detail_tag(detail, soup)
+    out_list = []
+    while detail_tag is not None:
+        out_list.append(detail_tag.text)
+        detail_tag = detail_tag.find_next_sibling()
     return out_list
 
 
@@ -96,9 +129,16 @@ def main():
         insert = pd.Series(data=data, index=TRULIA_FIRST_HEADERS)
         prelim_dataframe = prelim_dataframe.append(insert, ignore_index=True)
     driver.quit()
+    time.sleep(10)
     # Now, we want to get information about individual addresses, so look up the urls individually and parse them
-    for url in prelim_dataframe.url:
+    listing_url_dict = {}
+    # options = Options()
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    for entry_num, url in enumerate(prelim_dataframe.url):
+        print(f"Starting number {entry_num} url: {url}")
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(path=CACHE_DIR).install()))
+        driver.minimize_window()
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         # Get rent:
@@ -113,8 +153,25 @@ def main():
         # Get number of baths:
         baths_image = soup.find('img', attrs=TRULIA_HOUSE_METRICS_ATTRS['baths'])
         baths_numeric = float(baths_image.find_next_sibling().text.split()[0])
+        # Parse highlights:
         home_highlights_dict = parse_home_highlights(soup)
-        appliances_list = parse_appliances(soup)
+        # Check details:
+        details_to_parse = ['Appliances', 'Interior Details', 'Exterior Home Features']
+        listing_detail_dict = {}
+        for detail in details_to_parse:
+            listing_detail_dict[detail] = parse_detail_feature_list(detail, soup)
+        location_series = prelim_dataframe.loc[prelim_dataframe.url == url, ['streetAddress', 'addressLocality', 'addressRegion', 'postalCode']].iloc[0]
+
+        listing_url_dict[url] = HomeListing(address=location_series.streetAddress, listing_url=url,
+                                            town=location_series.addressLocality, zip=location_series.postalCode,
+                                            state=location_series.addressRegion, beds=beds_numeric,
+                                            baths=baths_numeric, sqft=sq_ft_numeric, listing_time=None, parking=None,
+                                            appliances=listing_detail_dict['Appliances'],
+                                            interior_features=listing_detail_dict['Interior Details'],
+                                            exterior_features=listing_detail_dict['Exterior Home Features'])
+        driver.quit()
+        time.sleep(2)
+
 
 
 
